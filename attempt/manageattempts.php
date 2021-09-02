575fb37c9147e9e5f9f2912bdb1f04005830bf90
justinhunt <bitwalkerjapan@gmail.com>
2021-09-02 03:24:02 +0000

    updated some bad instruction

--- attempt/manageattempts.php
+++ attempt/manageattempts.php
@@ -289,7 +289,7 @@ $PAGE->navbar->add(get_string('editingattempt', constants::M_COMPONENT, get_stri
 $mode='attempts';
 
 echo $renderer->header($moduleinstance, $cm,$mode, null, get_string('edit', constants::M_COMPONENT));
-echo $attempt_renderer->add_edit_page_links($moduleinstance, $attempt,$type);
+echo $attempt_renderer->add_edit_page_links($moduleinstance, $attempt,$type,$cm);
 echo html_writer::start_div(constants::M_COMPONENT .'_step' . $type);
 $mform->display();
 echo html_writer::end_div();
