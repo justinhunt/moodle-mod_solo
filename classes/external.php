2ab7a4f85cda36fc2e449eec48e838993e772e87
justinhunt <bitwalkerjapan@gmail.com>
2023-05-18 06:36:10 +0000

    revised use statement

--- classes/external.php
+++ classes/external.php
@@ -11,11 +11,11 @@ namespace mod_solo;
 
 
 use context_module;
-use external_api;
-use external_function_parameters;
-use external_multiple_structure;
-use external_single_structure;
-use external_value;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_value;
+use core_external\external_single_structure;
+use core_external\external_multiple_structure;
 use mod_solo\grades\gradesubmissions;
 use mod_solo\utils;
 use mod_solo\aitranscript;
