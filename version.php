3b91e8549eef18c50259b2923d1e22893ea409dd
justinhunt <bitwalkerjapan@gmail.com>
2023-06-14 09:00:38 +0000

    version bump

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   =  2023061400;
+$plugin->version   =  2023061401;
 $plugin->requires  = 2016052300;      // Requires Moodle 3.1
 $plugin->component = 'mod_solo';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.29 (Build 2023061400)';
+$plugin->release = '1.0.30 (Build 2023061401)';
 
