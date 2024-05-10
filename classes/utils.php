bd6a531188220488888385536caecd56f3de2d27
Justin Hunt <bitwalkerjapan@gmail.com>
2024-05-10 23:14:46 +0900

    added TTS voices

--- classes/utils.php
+++ classes/utils.php
@@ -1929,8 +1929,8 @@ class utils{
     public static function get_tts_voices($langcode='en-US',$showall=true) {
         $alllang = array(
             constants::M_LANG_ARAE => ['Zeina'=>'Zeina'],
+            constants::M_LANG_ARSA =>  ['Hala'=>'Hala','Zayd'=>'Zayd'],
             constants::M_LANG_BGBG => ['bg-BG-Standard-A' => 'Mila_g'],//nikolai
-            //constants::M_LANG_ARSA => [],
             constants::M_LANG_HRHR => ['hr-HR-Whisper-alloy'=>'Marko','hr-HR-Whisper-shimmer'=>'Ivana'],
             constants::M_LANG_CSCZ => ['cs-CZ-Wavenet-A' => 'Zuzana_g', 'cs-CZ-Standard-A' => 'Karolina_g'],
             constants::M_LANG_ZHCN => ['Zhiyu'=>'Zhiyu'],
