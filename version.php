a7d90a63dfd558dfb5e49392e2959772d6490b0c
Justin Hunt <bitwalkerjapan@gmail.com>
2024-05-16 22:12:33 +0900

    fixed issue with TTS player which could break page layout on some themes

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   =  2024051600;
+$plugin->version   =  2024051601;
 $plugin->requires  = 2016052300;      // Requires Moodle 3.1
 $plugin->component = 'mod_solo';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.47 (Build 2024051600)';
+$plugin->release = '1.0.48 (Build 2024051601)';
 
 
